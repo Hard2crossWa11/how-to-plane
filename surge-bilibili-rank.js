756fd5c638dfa67aa42a07eeeae901e93e962233
Hard2crossWa11 <57248664+Hard2crossWa11@users.noreply.github.com>
2019-12-18 12:20:35 +0800

    Create surge-bilibili-rank.js

---
+++ surge-bilibili-rank.js
@@ -0,0 +1,12 @@
+//Customize blacklist
+let blacklist=['共青团中央','广东共青团','浙江共青团','央视频',]
+
+let body = $response.body
+body=JSON.parse(body)
+body['data'].forEach((element, index)=> {
+   if(blacklist.includes(element['name'])){ 
+         body['data'].splice(index,1)  
+    }
+})
+body=JSON.stringify(body)
+$done({body})
